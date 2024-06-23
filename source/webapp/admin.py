6a6e9da3babcb77ec41de43480cea9d50129eb76
Dasha <grace-kramm666@gmail.com>
2024-06-24 01:08:48 +0600

    Добавление модели + миграция + регистрация в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from webapp.models import Task
+
+
 # Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'description', 'status', 'date_of_completion']
+    list_display_links = ['id', 'description']
+    list_filter = ['status']
+    search_fields = ['description']
+    fields = ['description', 'status', 'date_of_completion']
+
+
+admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
+class Task(models.Model):
+    description = models.TextField(null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=30, null=False, blank=False, verbose_name="Статус", default="new",
+                              choices=status_choices)
+    date_of_completion = models.DateField(null=True, blank=True, verbose_name="Дата выполнения", default=None)
+
+    def __str__(self):
+        return f"{self.pk}) {self.description} ({self.status})"
+
+    class Meta:
+        db_table = "tasks"
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
