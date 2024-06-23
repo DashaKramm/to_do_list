0292d4e1d735accb7892f92d21d6e7fd1c001523
Dasha <grace-kramm666@gmail.com>
2024-06-24 02:17:43 +0600

    создала view + url + шаблон для страницы с созданием новой задачи

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from webapp.views import index
+from webapp.views import index, create_task
 
 urlpatterns = [
     path('', index),
+    path('create/', create_task)
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,22 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-from webapp.models import Task
+from webapp.models import Task, status_choices
 
 
 # Create your views here.
 def index(request):
     tasks = Task.objects.order_by('-id')
     return render(request, 'index.html', context={'tasks': tasks})
+
+
+def create_task(request):
+    if request.method == "GET":
+        return render(request, "create_task.html", context={'status_choices': status_choices})
+    else:
+        Task.objects.create(
+            description=request.POST.get("description"),
+            status=request.POST.get("status"),
+            date_of_completion=request.POST.get("date_of_completion")
+        )
+        return HttpResponseRedirect("/")
