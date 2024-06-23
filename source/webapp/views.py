6ed430cf56d179fe18dd32efc945fb768b8cb278
Dasha <grace-kramm666@gmail.com>
2024-06-24 02:59:53 +0600

    исправила ошибку во view для удаления задачи, если дата осталась незаполненной

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -14,10 +14,15 @@ def create_task(request):
     if request.method == "GET":
         return render(request, "create_task.html", context={'status_choices': status_choices})
     else:
+        description = request.POST.get("description")
+        status = request.POST.get("status")
+        date_of_completion = request.POST.get("date_of_completion")
+        if not date_of_completion:
+            date_of_completion = None
         Task.objects.create(
-            description=request.POST.get("description"),
-            status=request.POST.get("status"),
-            date_of_completion=request.POST.get("date_of_completion")
+            description=description,
+            status=status,
+            date_of_completion=date_of_completion
         )
         return HttpResponseRedirect("/")
 
