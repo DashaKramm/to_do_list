56b9b6ef08c65e8e7a22a9eae24b010cc0cba62d
Dasha <grace-kramm666@gmail.com>
2024-06-24 01:40:39 +0600

    создала view + url + шаблон для главной страницы - просмотра списка задач

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+from webapp.models import Task
+
+
 # Create your views here.
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'index.html', context={'tasks': tasks})
