2d04cb9d69784d0db65025c943c77b717683233d
Dasha <grace-kramm666@gmail.com>
2024-07-04 01:34:36 +0600

    создала модельную форму задачи и использовала её на страницах создания и редактирования задачи

---
+++ source/webapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.forms import widgets
+
+from webapp.models import Task
+
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = '__all__'
+        widgets = {
+            'detailed_description': widgets.Textarea(attrs={"cols": 24, "rows": 5}),
+            'date_of_completion': widgets.DateInput(attrs={"type": "date"}),
+        }
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse
 
-from webapp.models import Task, status_choices
+from webapp.forms import TaskForm
+from webapp.models import Task
 
 
 # Create your views here.
@@ -13,21 +14,14 @@ def index(request):
 
 def create_task(request):
     if request.method == "GET":
-        return render(request, "create_task.html", context={'status_choices': status_choices})
+        form = TaskForm()
+        return render(request, "create_task.html", context={'form': form})
     else:
-        description = request.POST.get("description")
-        status = request.POST.get("status")
-        date_of_completion = request.POST.get("date_of_completion")
-        detailed_description = request.POST.get("detailed_description")
-        if not date_of_completion:
-            date_of_completion = None
-        task = Task.objects.create(
-            description=description,
-            status=status,
-            date_of_completion=date_of_completion,
-            detailed_description=detailed_description,
-        )
-        return redirect('detailed_task_view', pk=task.pk)
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = form.save()
+            return redirect('detailed_task_view', pk=task.pk)
+        return render(request, "create_task.html", context={'form': form})
 
 
 def delete_task(request, *args, pk, **kwargs):
@@ -47,14 +41,12 @@ def detailed_task_view(request, *args, pk, **kwargs):
 def update_task(request, *args, pk, **kwargs):
     task = get_object_or_404(Task, pk=pk)
     if request.method == "GET":
-        return render(request, "update_task.html", context={
-            'status_choices': status_choices,
-            'task': get_object_or_404(Task, pk=pk)
-        })
+        form = TaskForm(instance=task)
+        return render(request, "update_task.html", context={'form': form})
     else:
-        task.description = request.POST.get("description")
-        task.status = request.POST.get("status")
-        task.date_of_completion = request.POST.get("date_of_completion") or None
-        task.detailed_description = request.POST.get("detailed_description")
-        task.save()
-        return redirect('detailed_task_view', pk=task.pk)
+        form = TaskForm(data=request.POST, instance=task)
+        if form.is_valid():
+            task = form.save()
+            return redirect('detailed_task_view', pk=task.pk)
+        else:
+            return render(request, "update_task.html", context={'form': form})
