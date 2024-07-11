1bc79b40b1a3696c9ebbbf031fa6cefee45e6280
Dasha <grace-kramm666@gmail.com>
2024-07-11 17:33:38 +0600

    добавила RedirectView для главной страницы

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from webapp.views import TaskListView, TaskDetailView, CreateTaskView, UpdateTaskView, DeleteTaskView, \
     TasksListDeleteView
 
 urlpatterns = [
-    path('', TaskListView.as_view(), name='tasks'),
+    path('', RedirectView.as_view(pattern_name='tasks')),
+    path('tasks/', TaskListView.as_view(), name='tasks'),
     path('create/', CreateTaskView.as_view(), name='create_task'),
     path('task/<int:pk>/delete', DeleteTaskView.as_view(), name='delete_task'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='detailed_task_view'),
     path('task/<int:pk>/update', UpdateTaskView.as_view(), name='update_task'),
-    path('tasks/', TasksListDeleteView.as_view(), name='tasks_list'),
+    path('tasks_multiple_delete/', TasksListDeleteView.as_view(), name='tasks_list'),
 ]
